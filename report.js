89326405db1db7d804b92ad31df00096f9823d0f
Asif Shahriar <71173675+asif-shahriar@users.noreply.github.com>
2021-12-17 18:22:50 +0600

    Update report.js

--- report.js
+++ report.js
@@ -1,13 +1,13 @@
     const newman = require('newman');
     
     newman.run({
-        collection: require('./collection/customers_collection.json'), // pointing to local JSON file.
-        environment: require('./collection/customerEnv.json'), // pointing to local env file
+        collection: require('./collection/customers_collection.json'), 
+        environment: require('./collection/customerEnv.json'), 
         iterationCount: 1,
         reporters: 'htmlextra',
         reporter: {
             htmlextra: {
-                export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+                export: './Reports/report.html',
            }
        }
     }, function (err) {
